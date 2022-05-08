ca24286e1443933ba430de662f8681a82d322e0b
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2022-05-08 09:45:29 +0200

    auth setup

--- Controllers/BoardController.cs
+++ Controllers/BoardController.cs
@@ -1,9 +1,11 @@
 ﻿using board_service.DBContexts;
 using board_service.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace board_service.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("board")]
 public class BoardController : ControllerBase
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using board_service.DBContexts;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +28,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.Authority = builder.Configuration["Jwt:Authority"];
+    o.Audience = builder.Configuration["Jwt:Audience"];
+    o.Events = new JwtBearerEvents()
+    {
+        OnAuthenticationFailed = c =>
+        {
+            if (!c.Response.HasStarted)
+            {
+                c.NoResult();
+                c.Response.StatusCode = 500;
+                c.Response.ContentType = "text/plain";
+                return c.Response.WriteAsync("An error occured processing your authentication.");
+            }
+            else
+            {
+                return c.Response.WriteAsync(string.Empty);
+            }
+        }
+    };
+});
 
 var app = builder.Build();
 
